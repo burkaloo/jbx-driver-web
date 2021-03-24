b5ca88aa9d0913ab5bc4642b58dc6dd02d26c2a0
James Ong <burkedev@gmail.com>
2021-03-24 22:54:40 +0800

    default

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <QrcodeStream @decode="onDecode"></QrcodeStream>
+  <qrcode-stream @decode="onDecode"></qrcode-stream>
 </template>
 
 <script>
